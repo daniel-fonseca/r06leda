295e98a58e2ceba6c06bd277d5ea7bed25b751ec
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2021-04-23 10:02:46 -0300

    fiz um metodo para converter singlenodes para doublenodes, ainda nao testei como isso vai funcionar

--- src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -45,6 +45,28 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 			this.setLast(newLastNode);
 		}
 	}
+	
+	private void convertNodes() {
+		T[] values = super.toArray();
+		DoubleLinkedListNode<T>[] nodes = new DoubleLinkedListNode[values.length];
+		if (this.size() == 1) {
+			DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>(this.getHead().getData(), null, null);
+			this.setHead(newNode);
+			this.setLast(newNode);
+		}
+		
+		
+		else if (this.size() > 1) {
+			for (int i = 0; i < values.length; i++) {
+				nodes[i] = new DoubleLinkedListNode<T>(nodes[i].getData(), null, null);
+			}
+				
+			for (int i = 0; i < nodes.length - 1; i++) {
+				nodes[i].setNext(nodes[i+1]);
+				nodes[i+1].setPrevious(nodes[i]);
+			}
+		}
+	}
 
 	public DoubleLinkedListNode<T> getLast() {
 		return last;
